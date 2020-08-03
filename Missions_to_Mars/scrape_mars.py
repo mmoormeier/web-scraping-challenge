7c182885a10d3ccef52788913fd7bd802fae4911
mattmoormeier <matt@gpctechteam.com>
2020-08-03 15:31:03 -0400

    final

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup as bs
 from splinter import Browser
 import pandas as pd
 import re
+import time
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
@@ -26,21 +27,14 @@ def scrape_info():
     html = browser.html
     soup = bs(html, 'html.parser')
 
-    results = soup.find_all("div", class_= "image_and_description_container")
-
-    for result in results:
-        print("_______")
-        #print (result)
-        try:
-            print(result.find("div", class_='content_title').text)
-            news_title = (result.find("div", class_='content_title').text)
-        except:
-            print("")
-        try:
-            print(result.find("div", class_='article_teaser_body').text)
-            news_p = (result.find("div", class_='article_teaser_body').text)
-        except:
-            print("")
+    try:
+        slide = soup.select_one("ul.item_list li.slide")
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide.find("div", class_="content_title").get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide.find("div", class_="article_teaser_body").get_text()
+    except AttributeError:
+        return None, None
 
 
 
@@ -64,17 +58,12 @@ def scrape_info():
     browser.find_by_css('div[class="addthis_toolbox addthis_default_style"]')
 
     #build url query
-    url = browser.find_by_css('div[class="addthis_toolbox addthis_default_style"]').find_by_css('a').last['href']
+    new_url = browser.find_by_css('div[class="addthis_toolbox addthis_default_style"]').find_by_css('a').last['href']
 
     browser.find_by_css('div[class="addthis_toolbox addthis_default_style"]').find_by_css('a').last.click()
 
-    print(url)
-
-
     browser = init_browser()
-    browser.visit(url)
-
-
+    browser.visit(new_url)
 
     html = browser.html
     soup = bs(html, 'html.parser')
@@ -82,16 +71,14 @@ def scrape_info():
     p_tag = soup.find_all('aside')[0].find_all('p')
 
 
-    soup.find_all('aside')[0].find_all('p')[6].text
-
-
+    
     for p in p_tag:
         print(p)
         if 'Full-Res JPG' in p.text:
-            featured_image_url = p.a['href']
+            featured_image_url = 'https:'+p.a['href']
 
 
-    featured_image_url
+    
  
     # Mars Weather - Twitter
 
@@ -100,52 +87,49 @@ def scrape_info():
 
 
     browser = init_browser()
+    mars_weather = ''
     url = 'https://twitter.com/marswxreport?lang=en'
     browser.visit(url)
-
-
+    time.sleep(5)
+    browser.reload()
+    time.sleep(5)
     html = browser.html
+    time.sleep(5)
     soup = bs(html, 'html.parser')
-
-    results = soup.find_all('span')
-
-    # loop through list of tweets to find latest with weather data
-    y=0
+    time.sleep(5)
+    results = soup.find('div', class_= 'css-1dbjc4n').find_all('span', class_ ="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0")
     found = False
-
     for result in results:
-        y = y+1
-        if (('low' in result.text) & ('C' in result.text) & (not found)):
-            print(result)
-            print(y)
-            mars_weather = result.text
+        if (bool(result.find(string=re.compile("InSight"))) & (not found)):
+            mars_weather = result.find(string=re.compile("InSight"))
+            print(mars_weather)
             found = True
-
+    print(mars_weather)
 
        # Mars Facts
+    mars_facts_url = 'https://space-facts.com/mars/'
 
-    fact_url = 'https://space-facts.com/mars'
-    fact_list = pd.read_html(fact_url)
+    facts_list = pd.read_html(mars_facts_url)
 
-    fact_df = fact_list[0]
+    facts_df = facts_list[0]
 
-    fact_df.columns = ["measure", "value"]
+    facts_df.columns = ["measure", "value"]
+    
+    facts_df.set_index('measure', inplace = True)
+    
+    facts_df_html = facts_df.to_html(classes = "table")
 
-
-    fact_df.set_index('measure', inplace = True)
-    fact_df
-
+    facts_df.to_html('mars_fact_table.html')
+    
 
     # Mars Hemisphere
 
     executable_path = {'executable_path': 'chromedriver.exe'}
     browser = Browser('chrome', **executable_path, headless=False)
 
-
     browser = init_browser()
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
-
 
     html = browser.html
     soup = bs(html, 'html.parser')
@@ -165,24 +149,17 @@ def scrape_info():
     for result in results:
         x = x+1
         if result.find('a', class_ = "itemLink product-item"):
-            print("-------------------")
-            print(x)
-            print(result)
+           
             hemisphere_label = result.find_all('a', class_ = "itemLink product-item")[0].find('img')['alt']
             split_name = hemisphere_label.split()
             remove_words = ['Enhanced', 'thumbnail']
-            print(split_name)
+            
             result_words = [word for word in split_name if word not in remove_words]
             hemisphere_name.append(" ".join(result_words))
-            print(hemisphere_name)
-            print(result.find_all('a', class_ = "itemLink product-item")[0]['href'])
+            
             hemisphere_link.append(result.find_all('a', class_= "itemLink product-item")[0]['href'])
 
-
-
-    hemisphere_name
-
-    hemisphere_link
+ 
 
 
     for x in range(0,4):
@@ -193,17 +170,14 @@ def scrape_info():
         hidef_hemisphere_link.append(soup.find_all('ul')[0].find_all('li')[1].find('a')['href'])
 
 
-    hidef_hemisphere_link
+    
 
     # Put it all together
-    mars_data = {'news_title' : news_title, 'news_p' : news_p, 'featured_image_url' : featured_image_url, 'mars_weather' : mars_weather, 'hemisphere_name' : hemisphere_name, 'hidef_hemisphere_link' : hidef_hemisphere_link}
+    mars_data = {'news_title' : news_title, 'news_p' : news_p, 'featured_image_url' : featured_image_url, 'mars_weather' : mars_weather, 'mars_facts' : facts_df_html, 'hemisphere_name' : hemisphere_name, 'hidef_hemisphere_link' : hidef_hemisphere_link}
 
 
  # Close the browser after scraping
     browser.quit()
-
-
-
 
     return mars_data
 
